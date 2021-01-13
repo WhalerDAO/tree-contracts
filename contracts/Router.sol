dd05a4feb802d72f76651cfb6c517fa34794401e
Carl Farterson <carlfarterson@gmail.com>
2021-01-13 10:39:15 -0600

    Update: safemath multiplication before division

--- contracts/Router.sol
+++ contracts/Router.sol
@@ -41,6 +41,7 @@ contract Router is ReentrancyGuard {
     uint256 public charityCut = 100000000000000000;
 	uint256 public rewardsCut = 100000000000000000;
     address public gov = 0xade20A93179003300529AfeF3853F9679234D929;
+    address public reserve = 0x504397F81b1676710815f09CC3F3e1F3ee46c455;
     // address public charity = 0x703e0bEFF5d4f917b4b97ec8dC6D9ddbC90fb175;
     // I_TREERewards public lpRewards = I_TREERewards(0xe7B7d02296A016e25a40F18DFD55626A64308fE3);
     // I_OmniBridge public omniBridge = I_OmniBridge(0xc47192E48F75B612c792833C8b54Ad6BE319af90);
@@ -55,20 +56,21 @@ contract Router is ReentrancyGuard {
 		address[] calldata path,
 		address to,
 		uint deadline
-	) external returns (uint256[] memory amounts) {
-		require(msg.sender == RESERVE, 'UniswapV2Router: not reserve');
+	) external returns (uint256[2] memory amounts) {
+		// require(msg.sender == address(reserve), 'UniswapV2Router: not reserve');
 		require(deadline >= block.timestamp, 'UniswapV2Router: EXPIRED');
 
         // move oldReserveBalance to charity by reversing the code that computes the charityCutAmount
-        amounts[0] = 0;
-        amounts[1] = oldReserveBalance.div(charityCut).mul(PRECISION.sub(rewardsCut));
+        // NOTE: multiplying before dividing leads to less rounding :)
+        amounts[0] = 10000000;
+        amounts[1] = oldReserveBalance.mul(PRECISION.sub(rewardsCut)).div(charityCut);
 
         emit ReserveTransferred();
 	}
 
 
     function withdrawToken(address _token, address _to, uint256 _amount, bool max) external {
-        require(msg.sender == gov, "Router: not gov");
+        require(msg.sender == address(gov), "Router: not gov");
         if (max) {_amount = I_ERC20(_token).balanceOf(address(this));}
         I_ERC20(_token).transfer(_to, _amount);
         emit WithdrawToken(_token, _to, _amount);
