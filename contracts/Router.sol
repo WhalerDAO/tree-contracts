ce1412dd904f0e01a5fe3d706464ddaa7424665f
Carl Farterson <carlfarterson@gmail.com>
2021-02-13 09:23:54 -0800

    Fix: remove old code & comments

--- contracts/Router.sol
+++ contracts/Router.sol
@@ -32,8 +32,6 @@ contract Router is ReentrancyGuard {
     event SetReserveToken(address token);
     event WithdrawToken(address token, address to, uint256 amount);
 
-    // Constants are from the original Reserve
-    /// @notice precision for decimal calculations
     uint256 public constant PRECISION = 10**18;
 
     // Hardcoded data pulled from current reserve
@@ -41,10 +39,6 @@ contract Router is ReentrancyGuard {
     uint256 public charityCut = 100000000000000000;
 	uint256 public rewardsCut = 100000000000000000;
     address public gov = 0xade20A93179003300529AfeF3853F9679234D929;
-    address public reserve = 0x504397F81b1676710815f09CC3F3e1F3ee46c455;
-    // address public charity = 0x703e0bEFF5d4f917b4b97ec8dC6D9ddbC90fb175;
-    // I_TREERewards public lpRewards = I_TREERewards(0xe7B7d02296A016e25a40F18DFD55626A64308fE3);
-    // I_OmniBridge public omniBridge = I_OmniBridge(0xc47192E48F75B612c792833C8b54Ad6BE319af90);
     // Current DAI balance
     uint256 public oldReserveBalance = 17621554639972284767269;
     
@@ -56,9 +50,7 @@ contract Router is ReentrancyGuard {
 		address[] calldata path,
 		address to,
 		uint deadline
-	// ) external returns (uint256[] memory amounts) {
 	) external returns (uint256[] memory amounts) {
-		// require(msg.sender == address(reserve), 'UniswapV2Router: not reserve');
 		require(deadline >= block.timestamp, 'UniswapV2Router: EXPIRED');
 
         // move oldReserveBalance to charity by reversing the code that computes the charityCutAmount
@@ -71,12 +63,10 @@ contract Router is ReentrancyGuard {
         return amounts;
 	}
 
-
     function withdrawToken(address _token, address _to, uint256 _amount, bool max) external {
         require(msg.sender == address(gov), "Router: not gov");
         if (max) {_amount = I_ERC20(_token).balanceOf(address(this));}
         I_ERC20(_token).transfer(_to, _amount);
         emit WithdrawToken(_token, _to, _amount);
     }
-
 }
