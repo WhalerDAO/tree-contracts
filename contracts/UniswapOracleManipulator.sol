eba5a708be4c0b6cf052be36fe730d73f99779ef
Carl Farterson <carlfarterson@gmail.com>
2021-02-19 22:33:55 -0800

    Fix: clean up

--- contracts/UniswapOracleManipulator.sol
+++ contracts/UniswapOracleManipulator.sol
@@ -1,35 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.6.6;
-import "@openzeppelin/contracts/math/SafeMath.sol";
-
+pragma solidity 0.8.0;
 
 contract UniswapOracleManipulator {
-  using SafeMath for uint256;
-  bool public updated;
 
-  uint256 public price;
+    // TODO: minimum to trigger rebalance?
+  uint256 public price = 108 * 10**16;  // $1.08
 
-  constructor() public {
-    updated = true;
-    // from `price1CumulativeLast` in https://etherscan.io/address/0xcf70a458b86607ed65f03409f84bcb869e62538d#readContract
-    price = 108 * 10**16;  // $1.08
-  }
-
-  function setPrice(uint256 _price) public {
-    price = _price;
-  }
+  constructor() public {}
 
   function update() external pure returns (bool success) {
     return true;
   }
 
-  // token1 = TREE
   function consult(address token, uint256 amountIn)
     external
     view
     returns (uint256 amountOut)
   {
-    // amountOut = price.mul(amountIn);
     amountOut = price;
   }
 
